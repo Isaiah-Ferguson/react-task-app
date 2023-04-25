1d77bfd856089d015488c141bbe8cb66ed1d1dd8
Ulises <ulisses.ortega@hotmail.com>
2023-04-25 12:01:12 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './Components/LoginComponent/LoginComponent.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginComponent from './Components/LoginComponent/LoginComponent';
 import UsersLogin from './Components/LoginComponent/UsersLogin';
-
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import TaskTrackerComponent from './Components/TaskTrackerComponent/TaskTrackerComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Components/TaskTrackerComponent/TaskTrackerCard.css'
+
+
 function App() {
   return (
     <BrowserRouter>
