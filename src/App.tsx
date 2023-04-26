c12bb3caf55b20f5e9bdeff5553d4ca3acb387bb
Ulises <ulisses.ortega@hotmail.com>
2023-04-26 10:41:42 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
           <Route path='/' element={<LoginComponent/>}/>
           <Route path='/UsersLogin' element={<UsersLogin/>}/>
           <Route path='/CreateAccountComponent' element={<CreateAccountComponent/>} />
-                
         </Routes>
       </BrowserRouter>
   );
