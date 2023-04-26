3024a78ea578f8643c7aa7a731735a3d6c41c555
Ulises <ulisses.ortega@hotmail.com>
2023-04-26 10:49:41 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Components/TaskTrackerComponent/TaskTrackerCard.css'
 import './Components/LoginComponent/LoginComponent.css'
 import './Components/LoginComponent/UsersLogin.css'
+import CreateAccountComponent from './Components/CreateAccountComponent/CreateAccountComponent';
+
 
 
 
