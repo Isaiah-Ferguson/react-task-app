a38d30172069e4b15da6d8fd0de8045e80cfb66a
aisha mashrah <aishamashrah@gmail.com>
2023-04-26 11:51:04 -0700

    fixed css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import './App.css';
+
+import './Components/CreateAccountComponent/CreateAccountComponent.css'
+
+
 import CreateAccountComponent from './Components/CreateAccountComponent/CreateAccountComponent';
 import CreateAccountAdminComponent from './Components/CreateanAccountAdminComponent/CreateAccountAdminComponent';
 function App() {
