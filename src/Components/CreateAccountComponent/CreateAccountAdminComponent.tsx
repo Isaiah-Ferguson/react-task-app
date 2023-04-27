84be99149e73b053d16be7f84418a9a7c7f2d0ab
aisha mashrah <aishamashrah@gmail.com>
2023-04-27 10:37:09 -0700

    done

--- src/Components/CreateAccountComponent/CreateAccountAdminComponent.tsx
+++ src/Components/CreateAccountComponent/CreateAccountAdminComponent.tsx
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './CreateAccountComponent.css'
 import { createAccount } from "../../DataServices/DataServices";
 export default function CreateAccountAdminComponent() {
 
--- src/Components/CreateAccountComponent/CreateAccountComponent.tsx
+++ src/Components/CreateAccountComponent/CreateAccountComponent.tsx
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './CreateAccountComponent.css'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { createAccount } from "../../DataServices/DataServices";
@@ -25,19 +26,23 @@ navigate("/UsersLogin");
 }
 
   return (
+    <div className="background2">
       <Container>
-         <div className="background2">
+        
         <Row>
           <Col><div className="header">Create Account</div></Col>
-          <Col>
-            <Form className="form1">
+          
+        </Row>
+        <Row>
+        <Col>
+            <Form className="form1 text-center">
 
-              <Form.Group  controlId="formBasicEmail">
+              <Form.Group  controlId="formBasicEmail" className="input2">
                 <h1 className="inputtoptext">Username: </h1>
                 <Form.Control className="input" type="email" placeholder="Enter Username"  onChange={({ target: { value } }) => setUsername(value)}/>
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
+              <Form.Group controlId="formBasicPassword " className="input2 ">
                 <h1 className="inputtoptext">Password:</h1>
                 <Form.Control className="input" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}  />
               </Form.Group>
@@ -51,11 +56,10 @@ navigate("/UsersLogin");
           </Button>
           </div>
           </Col>
-
-
         </Row>
-        </div>
+        
       </Container>
+      </div>
 
     
 
